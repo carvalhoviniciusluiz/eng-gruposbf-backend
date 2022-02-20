afe99841186f28e5742d7c713952a77b354d56d8
Vinicius Carvalho <carvalho.viniciusluiz@gmail.com>
2022-02-20 01:34:19 -0300

    feat: ensure Logger#print if throws BcbApiException

--- src/converter/converter.controller.ts
+++ src/converter/converter.controller.ts
@@ -1,17 +1,27 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
 import { ApiBadRequestResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { AppLogger } from '~/app.logger';
+import { BcbApiException } from '~/bcb/exceptions';
 import { ConverterService } from '~/converter/converter.service';
 import { ConverterRequestDTO, ConverterResultDTO } from '~/converter/dtos';
 
 @ApiTags('v1')
 @Controller('converter')
 export class ConverterController {
-  constructor(private readonly converterService: ConverterService) {}
+  constructor(private readonly converterService: ConverterService, private readonly logger: AppLogger) {
+    this.logger.setContext(ConverterController.name);
+  }
 
   @Get()
   @ApiOkResponse({ description: 'Request accepted display results', type: ConverterResultDTO })
   @ApiBadRequestResponse({ description: 'Bad Request' })
   async converter(@Query() request: ConverterRequestDTO) {
-    return this.converterService.converter(request);
+    return this.converterService.converter(request).catch(error => {
+      const isBcbApiException = error instanceof BcbApiException;
+      if (isBcbApiException) {
+        this.logger.print({ category: error.message, error: error.stack });
+      }
+      throw new BadRequestException();
+    });
   }
 }
--- src/converter/converter.module.ts
+++ src/converter/converter.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { AppLogger } from '~/app.logger';
 import { BcbModule } from '~/bcb/bcb.module';
 import { ConverterController } from './converter.controller';
 import { ConverterService } from './converter.service';
@@ -6,6 +7,6 @@ import { ConverterService } from './converter.service';
 @Module({
   imports: [BcbModule],
   controllers: [ConverterController],
-  providers: [ConverterService]
+  providers: [ConverterService, AppLogger]
 })
 export class ConverterModule {}
